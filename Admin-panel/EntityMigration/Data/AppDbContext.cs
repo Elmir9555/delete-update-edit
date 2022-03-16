9e64a5f491687ff9dddbb5bc78c797ba6965d114
Elmir9555 <elmiriu@code.edu.az>
2022-03-16 16:53:26 +0400

    editdeletedreate

---
+++ Admin-panel/EntityMigration/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityMigration.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityMigration.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderDetail> SliderDetails { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<VideoValentine> Videos { get; set; }
+        public DbSet<Valentine> Valentines { get; set; }
+        public DbSet<Experts> Experts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<BlogCards> BlogCards { get; set; }
+        public DbSet<InstaCarousel> InstaCarousels { get; set; }
+
+
+
+    }
+}
